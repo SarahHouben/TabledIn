c4c73e7a2eeec8244dbd6ef209f443f7fb43c1ce
SarahHouben <sarahhouben@outlook.com>
2019-10-10 16:45:21 +0200

    commented cors

--- app.js
+++ app.js
@@ -51,11 +51,11 @@ app.use(
 );
 
 // CORS -  REQUIRED FOR CLOUDINARY might leave, not sure
-app.use(
-  cors({
-    origin: ["http://localhost:3000"]
-  })
-);
+// app.use(
+//   cors({
+//     origin: ["http://localhost:3000"]
+//   })
+// );
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "hbs");
